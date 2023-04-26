45c0b1d801e704ee978616226d8849e7d5d3fcd3
sheng020 <278324388@qq.com>
2023-04-26 23:33:40 +0800

    无逻辑修改

--- lib/features/chat/domain/entities/prompt_entitiy.dart
+++ lib/features/chat/domain/entities/prompt_entitiy.dart
@@ -1,33 +1,45 @@
-
-
-
 import 'package:equatable/equatable.dart';
 
-class PromptEntity  extends Equatable{
-
+class PromptEntity extends Equatable {
   final String? title;
 
   PromptEntity({this.title});
 
-
   @override
   List<Object?> get props => [title];
 
-
   static List<PromptEntity> exampleListData = [
-    PromptEntity(title: "Explain quantum computing in simple terms",),
-    PromptEntity(title: "Got any creative ideas for a 10 year old's birthday?",),
-    PromptEntity(title: "How do I make an HTTP request in javascript",),
+    PromptEntity(
+      title: "Explain quantum computing in simple terms",
+    ),
+    PromptEntity(
+      title: "Got any creative ideas for a 10 year old's birthday?",
+    ),
+    PromptEntity(
+      title: "How do I make an HTTP request in java",
+    ),
   ];
 
   static List<PromptEntity> capabilitiesListData = [
-    PromptEntity(title: "Remembers what user said earlier in the conversation",),
-    PromptEntity(title: "Allows user to provide follow-up corrections",),
-    PromptEntity(title: "Trained to decline inappropriate requests",),
+    PromptEntity(
+      title: "Remembers what user said earlier in the conversation",
+    ),
+    PromptEntity(
+      title: "Allows user to provide follow-up corrections",
+    ),
+    PromptEntity(
+      title: "Trained to decline inappropriate requests",
+    ),
   ];
   static List<PromptEntity> limitationListData = [
-    PromptEntity(title: "May occasionally generate incorrect information",),
-    PromptEntity(title: "May occasionally produce harmful instructions or biased content",),
-    PromptEntity(title: "Limited knowledge of world and events after 2021",),
+    PromptEntity(
+      title: "May occasionally generate incorrect information",
+    ),
+    PromptEntity(
+      title: "May occasionally produce harmful instructions or biased content",
+    ),
+    PromptEntity(
+      title: "Limited knowledge of world and events after 2021",
+    ),
   ];
 }
