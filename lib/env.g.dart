24d315559006eb993751cf39b7fa55012f21cd81
sheng020 <278324388@qq.com>
2023-05-29 21:58:14 +0800

    提交签名和服务器地址

---
+++ lib/env.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const DEFAULT_SERVER = 'https://openai.atominfo.services';
+  static const DEFAULT_KEY =
+      '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';
+}
